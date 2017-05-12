e7f3cc8d602ef86bb9e7ff4980a29966c5aff2b0
Tanvir <tanvir.tinz@gmail.com>
2017-05-12 19:11:00 -0400

    on feed forward

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,14 +1,18 @@
 from random import random, seed
 from Neuron import Neuron
 
-# seeding the random time to get similar random weights every time
-seed(1)
+seed(1) # seeding the random time to get similar random weights every time
 
 class NeuralNetwork(object):
+
 	def __init__(self, dimension):
-		self.layers = self.generateLayers(dimension)
+		self.layers = self.generate_layers(dimension)
 
 
+	'''
+		Purpose: Double underscore method called __repr__ to return a string
+				 which for a better representation of the Neural Network when printed.
+	'''
 	def __repr__(self):
 		string = ""
 		for i in range(len(self.layers)):
@@ -19,15 +23,16 @@ class NeuralNetwork(object):
 			string += "\n"
 		return string
 
-
-	def generateLayers(self, dimension):
+	'''
+		Purpose: Generates the layers of a Neural Network with the dimensions provided
+				 through the constructor.
+	'''
+	def generate_layers(self, dimension):
 		if isinstance(dimension, list):
 			self.layers = []
 			for i in range(len(dimension)):
 				list_of_neurons = [] # list of neurons in each layer
-
 				for j in range(dimension[i]): # dimension array contains how many neurons should be in each layer
-
 					if i - 1 > -1: # to stop going below 0th index, first layer should not have neurons with any weights
 						list_of_neurons.append(Neuron(len(self.layers[i - 1]), random)) # each neuron will contain a n number of weights in a list where n is the length of the neurons in the previous layer
 					
@@ -41,3 +46,16 @@ class NeuralNetwork(object):
 		else:
 			raise Exception("Invalid dimension type")
 		
+	'''
+		Purpose: A list of input is taken and is feed forwarded through the Neural Network.
+				 The input values follow sigma weight * input rule and then the neuron is
+				 activated using an activation function
+	'''
+	def feed_forward(self, input_list):
+		if not isinstance(input_list, list):
+			raise Exception("Input must be in the form of a list")
+
+
+		
+
+
--- Neuron.py
+++ Neuron.py
@@ -1,5 +1,6 @@
 
 class Neuron(object):
+
 	def __init__(self, num_weights, rand_func):
 		self.weights = self.gen_weights(num_weights, rand_func)
 		self.delta = 0
@@ -15,7 +16,7 @@ class Neuron(object):
 
 	def gen_weights(self, num_weights, rand_func):
 		self.weights = []
-		for i in range(num_weights):
+		for i in range(num_weights):	
 			self.weights.append(rand_func())
 
 		return self.weights
