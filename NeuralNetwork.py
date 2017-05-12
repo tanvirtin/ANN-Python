099afdbb9ee3e39c10f5d003848bda6ea175acff
Tanvir <tanvir.tinz@gmail.com>
2017-05-12 18:33:14 -0400

    initialized nn layers

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -11,23 +11,25 @@ class NeuralNetwork(object):
 
 	def __repr__(self):
 		string = ""
-
 		for i in range(len(self.layers)):
-			pass
-
+			string += "Layer" + str(i + 1) + "\n"
+			string += "-------\n"
+			for j in range(len(self.layers[i])):
+				string += str(self.layers[i][j])
+			string += "\n"
+		return string
 
 
 	def generateLayers(self, dimension):
 		if isinstance(dimension, list):
 			self.layers = []
 			for i in range(len(dimension)):
+				list_of_neurons = [] # list of neurons in each layer
 
 				for j in range(dimension[i]): # dimension array contains how many neurons should be in each layer
-					
-					list_of_neurons = [] # list of neurons in each layer
-					
+
 					if i - 1 > -1: # to stop going below 0th index, first layer should not have neurons with any weights
-						list_of_neurons.append(Neuron(len(self.layers[i - 1]), random))
+						list_of_neurons.append(Neuron(len(self.layers[i - 1]), random)) # each neuron will contain a n number of weights in a list where n is the length of the neurons in the previous layer
 					
 					else: # first layer, also known as input layer with no weights
 						list_of_neurons.append(Neuron(0, random))
--- __main__.py
+++ __main__.py
@@ -5,7 +5,9 @@ from Neuron import Neuron
 seed(1)
 
 def main():
-	nn = NeuralNetwork([1, 2, 1])
+	nn = NeuralNetwork([1, 2, 2, 1])
+
+	print(nn)
 
 if __name__ == "__main__":
 	main()
