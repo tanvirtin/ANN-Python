5fdff173cb7fa8960566f9f49daa25d333569fbe
tanvirtin <tanvir.tinz@gmail.com>
2017-10-28 14:47:15 -0400

    bug fix

--- __main__.py
+++ __main__.py
@@ -6,17 +6,19 @@ from Neuron import Neuron
 seed(1)
 
 def main():
-	nn = NeuralNetwork([1, 4, 4, 1], 0.5)
+	nn = NeuralNetwork([2, 4, 4, 1], 0.5)
 
 	epochs = 80000
 
-	for i in tqdm(range(epochs)):
-		nn.train([7], [1])
-		nn.train([10], [0])
+	for i in tqdm(range(10000)):
+		nn.train([0, 1], [0])
+		nn.train([0, 0], [0])
+		nn.train([1, 1], [1])
 
-	print(nn.query([10]))
 
-	print(nn.query([7]))
+	print(nn.query([1, 1]))
+	print(nn.query([0, 0]))
+	print(nn.query([0, 1]))
 
 if __name__ == "__main__":
 	main()
